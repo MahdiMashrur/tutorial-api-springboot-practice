5053d56cc14f0c7fe451d8aed39be05469f0fd30
MahdiMashrur <mashrur123@gmail.com>
2020-04-09 19:03:59 +0600

    a barebone spring-boot app. First commit

---
+++ src/main/java/io/mahdi/TutorialApiSpringbootPracticeApplication.java
@@ -0,0 +1,13 @@
+package io.mahdi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutorialApiSpringbootPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TutorialApiSpringbootPracticeApplication.class, args);
+	}
+
+}
