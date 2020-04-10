8a3c8d69c8c32b9201a5b18f04bbe0ea189c9e62
MahdiMashrur <mashrur123@gmail.com>
2020-04-10 15:58:15 +0600

    Adding constructor and getter-setters in the tutorial class: for being handled by spring

--- src/main/java/io/mahdi/tutorial/Tutorial.java
+++ src/main/java/io/mahdi/tutorial/Tutorial.java
@@ -5,6 +5,42 @@ import javax.persistence.Id;
 
 @Entity
 public class Tutorial {
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public boolean isIspublished() {
+		return ispublished;
+	}
+	public void setIspublished(boolean ispublished) {
+		this.ispublished = ispublished;
+	}
+	
+	public Tutorial( ) {
+		 
+	}
+	
+	public Tutorial(String id, String title, String description, boolean ispublished) {
+		super();
+		this.id = id;
+		this.title = title;
+		this.description = description;
+		this.ispublished = ispublished;
+	}
 	@Id
 	private String id;
 	private String title;
